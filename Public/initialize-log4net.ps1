af9c79b64e6ef45933f9fb46c76c180902988611
Lars Slouborg Bengtsson <LABE@danskespil.dk>
2017-12-28 16:54:29 +0100

    Big change to log4net initializor. More control over params. Bump version.

--- Public/initialize-log4net.ps1
+++ Public/initialize-log4net.ps1
@@ -8,10 +8,10 @@ function initialize-log4net() {
     Made into helper function by: Lars S. B. - bingchonglars@gmail.com - +4527320102
 .DESCRIPTION
     This helper function provides a practical way to log exceptions and so forth to the filesystem.
-.PARAMETER configFileName
-    The name of the log4net config file (XML).
-.PARAMETER log4NetPath
-    The path where log4net should look for and place files.
+.PARAMETER log4NetConfigFile
+    A fully qualified path to the Log4Net config (XML) file.
+.PARAMETER LogFilesPath
+    The path to the directory where logfiles will be written.
 .PARAMETER logfileName
     The name that the log4net logfile should get.
 .PARAMETER loggerName
@@ -19,23 +19,23 @@ function initialize-log4net() {
 .NOTES
     - The module expects that you configure log4net via an xml file. Use the provided template as a startingpoint. Found in the "config" folder.
         -- You can configure several loggers in the xml file. Call the one you want. It could e.g. be one with LEVEL DEBUG or one with LEVEL ERROR.
-    - The module returns a log4net logger object. Instantiated with the confguration settings in the injected xml file.
-    - Log4Net will not itself throw an error as it fails silently.
+    - The module returns a log4net logger object. Instantiated with the confguration settings in the xml file.
+    - Log4Net will not itself throw an error as it fails silently!
 .EXAMPLE
-    $log4netLogger = initialize-log4net -log4NetPath $log4NetPath -configFile $configFile -logfileName log4netlog -loggerName log4netLogName
+    $log4netLogger = initialize-log4net -log4NetConfigFile $log4NetConfigFile -logfileName $log4netlog -loggerName $log4netLogName -LogFilesPath $LogFilesPath
+    > Instantiates log4net with the settings found in the log4NetConfigFile specified. And with a logfilename as in $log4netlog and so forth.
 #>
 
     # Define parameters
     param(
-        [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="The path where log4net should look for and place files.")]
+        [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="A fully qualified path to the Log4Net config (XML) file.")]
         [ValidateNotNullOrEmpty()]
-        [String]$log4NetPath,
-        [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="The name of the log4net config file (XML).")]
-        [ValidateNotNullOrEmpty()]
-        [String]$configFileName,
+        [String]$log4NetConfigFile,
         [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="The name that the log4net logfile should get.")]
         [ValidateNotNullOrEmpty()]
         [String]$logFileName,
+        [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="The path to the directory where logfiles will be written.")]
+        [String]$LogFilesPath,
         [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="The name of the logger defined in the log4net config file.")]
         [ValidateNotNullOrEmpty()]
         [String]$loggerName
@@ -51,8 +51,8 @@ function initialize-log4net() {
     $log4NetLogManager = [log4net.logmanager]
 
     # Configure log4Net
-    [log4net.GlobalContext]::Properties["LogFileName"] = "$log4NetPath\$logFileName.log"
-    [log4net.Config.XmlConfigurator]::ConfigureAndWatch("$log4NetPath\$configFileName.xml")
+    [log4net.GlobalContext]::Properties["LogFileName"] = "$LogFilesPath\$logFileName.log"
+    [log4net.Config.XmlConfigurator]::ConfigureAndWatch("$log4NetConfigFile")
 
     # Instantiate a log4Net logger that will base its settings on the config in the provided .xml file
     $log4NetLogger = $log4NetLogManager::GetLogger($loggerName)
