ce6f38a32959910529bc79307b83bc8b572865b6
Lars Slouborg Bengtsson <LABE@danskespil.dk>
2018-01-15 15:27:09 +0100

    Bug in Set-xScheduledTask
    - Hardening so that errors become terminating.
    - User parameter on Set-ScheduledTask as this is an requirement.

--- Public/JobScheduling/Set-xScheduledTask.ps1
+++ Public/JobScheduling/Set-xScheduledTask.ps1
@@ -1,33 +1,40 @@
 function Set-xScheduledTask() {
 <#
 .DESCRIPTION
-    Long description
+    Configures the username:password combo on a Scheduled task.
 .INPUTS
-    Inputs (if any)
+    [CimInstance] representing the Scheduled task to configure.
+    [String] one for the password and another for the username.
 .OUTPUTS
-    [Boolean] relative to the result of setting
+    [Boolean] relative to the result of configuring the scheduled task.
 .NOTES
-    General notes
+    <none>
 .EXAMPLE
-    PS C:\> <example usage>
-    Explanation of what the example does
+    $result = Set-xScheduledTask -InputObject $task -UserName "HealOps" -Password "MyNotSoSecretPassword"
+        > Calls Set-xScheduledTask in order to configure the username:password combo on the Scheduled task in -InputObject.
 .PARAMETER InputObject
     A CimInstance Scheduled Task object.
 .PARAMETER Password
     A cleartext password to set on the a scheduled task.
+.PARAMETER UserName
+    The name of the user on the task to configure.
 #>
 
     # Define parameters
     [CmdletBinding()]
     [OutputType([Boolean])]
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword","")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingUserNameAndPassWordParams","")]
     param(
         [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="A CimInstance Scheduled Task object.")]
         [ValidateNotNullOrEmpty()]
         [CimInstance]$InputObject,
-        [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="A cleartext password to set on the a scheduled task.")]
+        [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="A cleartext password to set on the scheduled task.")]
         [ValidateNotNullOrEmpty()]
-        [String]$Password
+        [String]$Password,
+        [Parameter(Mandatory=$true, ParameterSetName="Default", HelpMessage="The name of the user on the task to configure.")]
+        [ValidateNotNullOrEmpty()]
+        [String]$UserName
     )
 
     #############
@@ -43,7 +50,7 @@ function Set-xScheduledTask() {
         if ($null -ne (Get-Module -Name ScheduledTasks -ListAvailable) ) {
             # ScheduledTasks PowerShell module
             try {
-                Set-ScheduledTask -InputObject $InputObject -Password $Password
+                Set-ScheduledTask -InputObject $InputObject -Password $Password -User $UserName -ErrorAction Stop
                 $result = $true
             } catch {
                 throw "Set-xScheduledTask > Did not successfully set the password on the job. Failed with > $_"
