4876b6ce3f5e6ee0661cec6d8b96128a8d2fcc16
Lars Bengtsson <bingchonglars@gmail.com>
2018-05-05 15:23:13 +0200

    Update vs code color settings and correct bug in syntax Pester test script used in a pre-commit hook.

--- Tests/psSyntaxAndImportValidation.pester.tests.ps1
+++ Tests/psSyntaxAndImportValidation.pester.tests.ps1
@@ -2,7 +2,7 @@
 Import-Module -name $PSScriptRoot/settings.pester.tests.psm1 -Force
 
 # Define the folders to look through
-$functionFolders = @('Public', 'Private')
+$functionFolders = @('Private', 'Public')
 
 # Define vars
 $moduleName = $($Settings.moduleName)
@@ -12,17 +12,19 @@ Describe "General project validation: $moduleName" {
         $folderPath = Join-Path -Path $moduleRoot -ChildPath $folder
         $scripts = Get-ChildItem $folderPath -Include *.ps1, *.psm1, *.psd1 -Recurse
 
-        # TestCases are splatted to the script so we need hashtables
-        $testCase = $scripts | Foreach-Object {@{file = $_}}
-        It "Script <file> should be valid powershell" -TestCases $testCase {
-            param($file)
+        if ($null -ne $scripts) {
+            # TestCases are splatted to the script so we need hashtables
+            $testCase = $scripts | Foreach-Object {@{file = $_}}
+            It "Script <file> should be valid powershell" -TestCases $testCase {
+                param($file)
 
-            $file.fullname | Should Exist
+                $file.fullname | Should Exist
 
-            $contents = Get-Content -Path $file.fullname -ErrorAction Stop
-            $errors = $null
-            $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
-            $errors.Count | Should Be 0
+                $contents = Get-Content -Path $file.fullname -ErrorAction Stop
+                $errors = $null
+                $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
+                $errors.Count | Should Be 0
+            }
         }
     }
 
