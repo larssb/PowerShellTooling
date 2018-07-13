a7c757a21b45f876d564b7ca31781e6a139e9cc9
Lars Bengtsson <bingchonglars@gmail.com>
2018-07-13 12:25:30 +0200

    Working on out-functionhierarchy. Major improvements.

--- Public/PowerShellInfo/out-functionHierarchy.ps1
+++ Public/PowerShellInfo/out-functionHierarchy.ps1
@@ -1,5 +1,5 @@
-##Requires -Module PSGraph
-function out-functionHierarchy() {
+#Requires -Module PSGraph
+function Out-FunctionHierarchy() {
 <#
 .DESCRIPTION
     Can be used to generate a graph over the hierarchy of the functions in a PowerShell project. Derived by parsing the functions from the
@@ -13,115 +13,218 @@ function out-functionHierarchy() {
 .EXAMPLE
     out-functionHierarchy -pathToPSD1file .\myModule.psd1;
     This will generate a function hierarchy graphviz graph over the myModule PowerShell module.
-.PARAMETER pathToPSD1file
-    Full path to the PSD1 manifest file.
-.PARAMETER pathToStoreGraph
+.PARAMETER ModuleName
+    The name of the module to analyze. Assumes that the module is installed in one of the default PowerShell module installation paths.
+.PARAMETER ModuleRoot
+    Full path to the root folder of the PowerShell module.
+.PARAMETER PathToStoreGraph
     The path to store the generated function hierarchy graph.
 #>
 
     # Define parameters
-    [CmdletBinding()]param(
-        [Parameter(Mandatory=$true, ParameterSetName="default", HelpMessage="Full path to the PSD1 manifest file.")]
+    [CmdletBinding(DefaultParameterSetName = "Default")]
+    [OutputType([Void])]
+    param(
+        [Parameter(Mandatory, ParameterSetName="ByModuleName")]
         [ValidateNotNullOrEmpty()]
-        [string]$pathToPSD1file,
-        [Parameter(Mandatory=$false, ParameterSetName="default", HelpMessage="The path to store the generated function hierarchy graph.")]
+        [String]$ModuleName,
+        [Parameter(Mandatory, ParameterSetName="ByModuleRoot")]
+        [ValidateNotNullOrEmpty()]
+        [string]$ModuleRoot,
+        [Parameter()]
         [string]$pathToStoreGraph
     )
 
     #############
     # Execution #
     #############
-    <#
-        - PREP
-    #>
-    Import-Module -Name PSGraph;
-    . $PSScriptRoot\get-environmentOS.ps1;
-    $rootOfTheProject = $pathToPSD1file -replace "\\(?!.*\\).+","";
+    Begin {
+        #Import-Module -Name PSGraph
+        . $PSScriptRoot\..\SystemInfo\get-environmentOS.ps1
+        #$rootOfTheProject = $pathToPSD1file -replace "\\(?!.*\\).+",""
 
-    <#
-        - Gather data on the PowerShell project. For later analyzation and output as a function hierarchy graph via Graphviz.
-    #>
-    # Import the specified module
-    $module = Import-Module -Name $pathToPSD1file -PassThru;
-
-    # Get the root module
-    $moduleRoot = $module.RootModule;
-    $moduleRootBaseName = $moduleRoot -replace "\..+",""; # Removes .psm1 from the module root name.
-
-    # Get the exported functions (commands), these are the public functions.
-    $moduleExportedCommands = $module.ExportedCommands;
-
-    # Iterate over each function to find the function/s it depends on
-    foreach ($exportedCommand in $exportedCommands) {
-        # Tokenize the function in order to be able to parse the Abstract Syntax Tree
-        $ast = [System.Management.Automation.PSParser]::Tokenize( (Get-Content $pathToFunction), [ref]$null);
-
-        # Look for dot-sourcing usage
-        $dotSourcedFiles = $ast.where( {$_.content -eq "." -and $_.Type -eq "Operator"} );
-
-        # Go over all lines with dot-sourcing calls
-        foreach ($dotSourcedFile in $dotSourcedFiles) {
-            #
-            $dotSourcedFile.startline
+        if ($PSBoundParameters.ContainsKey('ModuleName')) {
+            # Import the specified module by name. Therefore, loaded from one of the default PowerShell module path locations.
+            $Module = Import-Module -DisableNameChecking -Name $ModuleName -PassThru
+        } else {
+            # Import the specified module by its fullname/path.
+            $Module = Import-Module -DisableNameChecking -Name $ModuleRoot -PassThru
 
         }
 
-        # Look for import-module usage
-        $importedModules = $ast.where( {$_.content -eq "Import-module" -and $_.Type -eq "Command"} )
+        # Collection to hold private functions
+        [System.Collections.ArrayList]$PrivateFunctions = New-Object System.Collections.ArrayList
 
-        # Iterate over all lines with import-module calls
-        foreach ($importedModule in $importedModules) {
-            # Get the file being imported to go further down the hierarchy rabbit hole.
-            $importedFileInfo = $ast.where( {$_.startline -eq $importedModules.startline -and $_.Type -eq "CommandArgument" } );
-            $importedFileName = $importedFileInfo.Content -replace ".+\\",""
-            $importedFileLocation = Get-ChildItem -Path $rootOfTheProject -Recurse -Filter "$importedFileName*";
+        # Collection to hold the call hierarchy of the analyzed module
+        [System.Collections.ArrayList]$CallGraphObjects = New-Object System.Collections.ArrayList
+    }
+    Process {
+        <#
+            - Get the private functions in the module. So that we can distinguise these from the external cmdlets/functions used by the module
+        #>
+        # Get all PS1 files
+        $PS1Files = (Get-ChildItem -Path $Module.ModuleBase -Filter '*.ps1' -Recurse)
+        Write-Verbose -Message "All *.ps1 files retrieved > $($PS1Files | Out-String)"
 
-            # Get the AST data for the imported file
-            $ast = [System.Management.Automation.PSParser]::Tokenize( (Get-Content $importedFileLocation), [ref]$null);
+        # Run through all the retrieved *.PS1 files >> to identify declared functions in them
+        foreach ($PS1File in $PS1Files) {
+            $ast = [System.Management.Automation.PSParser]::Tokenize( (Get-Content -Path $PS1File.FullName), [ref]$null)
 
+            # Identify the declared functions in the file
+            $DeclaredFunctions = $ast.where( { $_.Type -eq "Keyword" -and $_.Content -eq "function" } )
+            Write-Verbose -Message "There is $($DeclaredFunctions.Count) declared functions in the file named $($PS1File.Name)."
 
+            foreach ($DeclaredFunction in $DeclaredFunctions) {
+                # Derive the name of the declared function
+                [String]$FunctionName = ($ast.where( { $_.Startline -eq $DeclaredFunction.StartLine -and $_.Type -eq "CommandArgument" } )).Content
+
+                # Control if the function is a public function in the module being analyzed
+                if (-not $PublicFunctions.Name.Contains($FunctionName)) {
+                    $PrivateFunctions.Add($FunctionName) | Out-Null
+                    Write-Verbose -Message "Found a private function named $FunctionName"
+
+                    # Parse the AST of the private function to find the CommandArguments used
+
+                } else {
+                    Write-Verbose -Message "The function named $FunctionName is a public function"
+                }
+            }
         }
-    }
 
-    <#
-        - Generate data for the graph
-    #>
-    $graphData = Graph psFunctionHierarchy {
-        # Exported commands
-        $exportedCommandNames = $moduleExportedCommands.Values.Name;
+        <#
+            Public functions.
+        #>
+        # Get the public functions loaded by the module
+        $PublicFunctions = Get-Command -Module $Module.Name
+        Write-Verbose -Message "The public functions retrieved > $($PublicFunctions | Out-String)"
 
-        Node projectRoot @{label="$moduleRootBaseName";shape='invhouse'}
-        edge projectRoot -To "public functions" @{shape='oval'}
-        edge projectRoot -To "private functions" @{shape='oval'}
-        Node $exportedCommandNames -NodeScript { "$_()" } @{label={"$_()"};shape='note'}
-        edge $exportedCommandNames -FromScript { "public functions" } -ToScript { "$_()" }
-    }
+        # Parse the AST of the public funtions to discover the CommandArguments used
+        foreach ($PublicFunction in $PublicFunctions) {
+            # Collection to hold the commands used by the function. Ordered to reflect the point-in-time of each commad invocation.
+            [System.Collections.ArrayList]$PublicFunctionCommandHierarchy = New-Object System.Collections.ArrayList
 
-    <#
+            # Tokenize the AST
+            $ast = [System.Management.Automation.PSParser]::Tokenize( $($PublicFunction.Definition), [ref]$null)
+
+            # Get the commands used in the code (references to other functions/cmdlets in the code)
+            $CommandsUsed = $ast.where( { $_.Type -eq "Command" } )
+
+            if ($null -ne $CommandsUsed) {
+                [System.Collections.ArrayList]$CommandsUsedInfo = New-Object System.Collections.Specialized.OrderedDictionary
+                foreach ($Command in $CommandsUsed) {
+                    # Control if it is a private function in the module
+                    $IsPrivateCommand = $PrivateFunctions.Contains($Command)
+                    if ($IsPrivateCommand) {
+                        [String]$CommandScope = "Private"
+                    }
+
+                    # Control if it is a public function in the module
+                    $IsPublicCommand = $PublicFunctions.Contains($Command)
+                    if ($IsPublicCommand) {
+                        [String]$CommandScope = "Public"
+                    }
+
+                    # Is the command defined in an external module
+                    if (-not $IsPrivateCommand -and -not $IsPublicCommand) {
+                        [String]$CommandScope = "External"
+                    }
+
+                    # Create a custom object to hold the info on the command analyzed
+                    $CommandInfo = @{
+                        "CommandName" = $Command
+                        "CommandScope" = $CommandScope
+                    }
+                    $CommandInfoCustomObject = New-Object -TypeName PSCustomObject -Property $CommandInfo
+
+                    # Add the command info to the collection
+                    $CommandsUsedInfo.Add($CommandInfoCustomObject)
+                }
+
+                # Add the analyzed info to the collection that holds all the aggregated info, derived by analyzing the public function/command currently being iterated over
+                $PublicFunctionCommandHierarchy.Add(@{
+                    "PublicFunctionAffiliation" = $PublicFunction.Name
+                    "Commands" = $CommandsUsedInfo
+                }) | Out-Null
+            }
+
+            # Add the result of analyzing the Public function to the CallGraphObjects collection
+            $CallGraphObjects.Add($PublicFunctionCommandHierarchy) | Out-Null
+        }
+
+        # Iterate over each function to find the function/s it depends on
+<#         foreach ($exportedCommand in $exportedCommands) {
+            # Tokenize the function in order to be able to parse the Abstract Syntax Tree
+            $ast = [System.Management.Automation.PSParser]::Tokenize( (Get-Content $pathToFunction), [ref]$null);
+
+            # Look for dot-sourcing usage
+            $dotSourcedFiles = $ast.where( {$_.content -eq "." -and $_.Type -eq "Operator"} );
+
+            # Go over all lines with dot-sourcing calls
+            foreach ($dotSourcedFile in $dotSourcedFiles) {
+                #
+                $dotSourcedFile.startline
+
+            }
+
+            # Look for import-module usage
+            $importedModules = $ast.where( {$_.content -eq "Import-module" -and $_.Type -eq "Command"} )
+
+            # Iterate over all lines with import-module calls
+            foreach ($importedModule in $importedModules) {
+                # Get the file being imported to go further down the hierarchy rabbit hole.
+                $importedFileInfo = $ast.where( {$_.startline -eq $importedModules.startline -and $_.Type -eq "CommandArgument" } );
+                $importedFileName = $importedFileInfo.Content -replace ".+\\",""
+                $importedFileLocation = Get-ChildItem -Path $rootOfTheProject -Recurse -Filter "$importedFileName*";
+
+                # Get the AST data for the imported file
+                $ast = [System.Management.Automation.PSParser]::Tokenize( (Get-Content $importedFileLocation), [ref]$null);
+
+
+            }
+        } #>
+
+        <#
+            - Generate data for the graph
+        #>
+        $graphData = Graph ModuleCallGraph {
+            Node -Name ProjectRoot @{label="$($Module.Name)";shape='invhouse'}
+            #edge ProjectRoot -To "Public Functions" @{shape='oval'}
+            #edge projectRoot -To "private functions" @{shape='oval'}
+            #Node -Name $exportedCommandNames -NodeScript { $_.Name } @{label = { $_.Name }; shape = 'note'}
+
+
+            foreach ($CallGraphObject in $CallGraphObjects) {
+
+                foreach ($Command in $CommandInfo) {
+
+                }
+                Edge $CallGraphObject -FromScript { $Module.Name } -ToScript { $_.Name }
+            }
+        }
+
+        <#
         - Build and export the graph.
-    #>
-    <#
-    if (-Not $PSBoundParameters.ContainsKey('pathToStoreGraph')) {
-        $file = [System.IO.Path]::GetRandomFileName();
-        $PSBoundParameters["DestinationPath"] = Join-Path $env:temp "$file.$OutputFormat";
+        #>
+        <#
+        if (-Not $PSBoundParameters.ContainsKey('pathToStoreGraph')) {
+            $file = [System.IO.Path]::GetRandomFileName();
+            $PSBoundParameters["DestinationPath"] = Join-Path $env:temp "$file.$OutputFormat";
+        }
+        #>
+
+        # Handle issue with path param in Export-PSGraph when on MacOS or Linux.
+        $os = get-environmentOS;
+
+        # Output the graph
+        if ($os -eq "Windows") {
+            $graphvizOutput = $graphData | Export-PSGraph -ShowGraph;
+        } else {
+            $file = [System.IO.Path]::GetRandomFileName();
+            $destinationPath = Join-Path $home "$file.png";
+            $graphvizOutput = $graphData | Export-PSGraph -DestinationPath $destinationPath;
+            Write-Output "Find the generated graph here: $destinationPath";
+        }
+
+        #$graphvizOutput.Fullname
     }
-    #>
-
-    # Handle issue with path param in Export-PSGraph when on MacOS or Linux.
-    $os = get-environmentOS;
-
-    # Output the graph
-    if ($os -eq "Windows") {
-        $graphvizOutput = $graphData | Export-PSGraph -ShowGraph;
-    } else {
-        $file = [System.IO.Path]::GetRandomFileName();
-        $destinationPath = Join-Path $home "$file.png";
-        $graphvizOutput = $graphData | Export-PSGraph -DestinationPath $destinationPath;
-        Write-Output "Find the generated graph here: $destinationPath";
-    }
-
-    #$graphvizOutput.Fullname
 }
-##################
-# FUNCTION - END #
-##################
